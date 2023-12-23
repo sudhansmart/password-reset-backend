e25e2e33a898568b6f639ad4888ef3f07b1871a4
sudhansmart <136149970+sudhansmart@users.noreply.github.com>
2023-12-23 14:56:39 +0530

    forgotpassword.js

--- controllers/forgotpassword.js
+++ controllers/forgotpassword.js
@@ -10,7 +10,7 @@ async function forgotpassword(email) {
     const user = await User.findOne({ email });
     user.forgetPassword = token;
 
-    const resetLink = `http://localhost:5173/resetpassword/${token}`;
+    const resetLink = `https://6586a6c756d7346ce1d1a1ba--passwordresetfront.netlify.app/resetpassword/${token}`;
     const content = `<h4>Hello Sir/Madam,</h4>
                      <h5>Welcome to My Page</h5>
                      <p>Click on the below link to Reset Password</p>
